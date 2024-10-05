048f4b386e48e0995778ab8e82637928aa8c24e6
Nesmy Point du Jour <pnesmy@gmail.com>
2024-10-05 06:38:36 -0400

    Delete out directory

--- out/vendor/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nesmy/Playground/Tetris/vendor/raylib
-# Build directory: /home/nesmy/Playground/Tetris/out/vendor/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
